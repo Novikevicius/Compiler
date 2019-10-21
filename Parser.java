c6ff7bce3e20ef311a154e29db2dba5064b10aca
Edvardas Novikevicius <edvardas.novikevicius@gmail.com>
2019-10-21 18:14:54 +0300

    changed error msg; added first syntax analyze

--- Parser.java
+++ Parser.java
@@ -13,21 +13,15 @@ public class Parser {
         this.fileName = fileName;
         offset = 0;
     }
-    public void parse()
-    {
-        while (accept(State.EOF) == null) {
-            expect(State.EOF);            
-        }
-
-    }
-    private void error() {error("Unexpected token");}
-    private void error(String msg)
+    private void error() {error("Unexpected token", null);}
+    private void error(String msg, State expectedType)
     {
         Token t = tokens.get(offset);
         int line = t.getLine();
         int column = t.getColumn();
         String[] array = fileName.split("/"); // get file name
-        throw new Error("ParserError:" + array[array.length-1] + ":" + line + ":" + column + ": " + msg);
+        String expected = expectedType == null ? "" : " Expected:" + String.format("%s", expectedType) + " got: " + String.format("%s",curToken.getType());
+        throw new Error("ParserError:" + array[array.length-1] + ":" + line + ":" + column + ": " + msg + expected);
     }
     private void expect(State tokenType)
     {
@@ -38,7 +32,7 @@ public class Parser {
         }
         else
         {
-            error();
+            error("Unexpected token error. ", tokenType);
         }
     }
     private State accept(State tokenType)
@@ -50,5 +44,51 @@ public class Parser {
             return curToken.getType();
         }
         return null;
+    }
+    // <start> ::= <func_declarations> EOF
+    public void parse()
+    {
+        while (accept(State.EOF) == null) {
+            parse_def_fns();          
+        }
+
+    }
+    // <func_declarations> ::= { <func_declaration> }
+    private void parse_def_fns()
+    {
+        parse_def_fn();
+    }
+    // <func_declaration>  ::= <return_type> <identifier> "(" <fnc_params> ")" <block> 
+    private void parse_def_fn()
+    {
+        parse_return_type();
+        expect(State.IDENTIFIER);
+        expect(State.L_PARENT);
+        parse_params();
+        expect(State.R_PARENT);
+        parse_block();
+    }
+    private void parse_return_type()
+    {
+        switch (curToken.getType()) {
+            case TYPE_VOID:
+                expect(State.TYPE_VOID);
+                break;
+            case TYPE_INT:
+                expect(State.TYPE_INT);
+                break;
+            default:
+                error();
+                break;
+        }
+    }
+    private void parse_params()
+    {
+        // do nothing, yet TODO:
+    }
+    private void parse_block()
+    {
+        expect(State.L_CURLY);
+        expect(State.R_CURLY);
     }
 }
