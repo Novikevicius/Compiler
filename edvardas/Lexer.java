582297a445983ba4a20d1607b3a1d842f6f33143
Edvardas Novikevicius <edvardas.novikevicius@gmail.com>
2019-11-01 15:41:49 +0200

    removed unused imports

--- edvardas/Lexer.java
+++ edvardas/Lexer.java
@@ -1,10 +1,7 @@
 package edvardas;
 
-import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.*;
 import java.lang.Math;
 
