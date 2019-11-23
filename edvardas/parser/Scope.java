786d87a68bcde64dd1b048b96df805eef285d85a
Edvardas Novikevicius <edvardas.novikevicius@gmail.com>
2019-11-23 17:22:16 +0200

    added resolveName, changed hashmap type to String

--- edvardas/parser/Scope.java
+++ edvardas/parser/Scope.java
@@ -7,7 +7,7 @@ import edvardas.ast.nodes.Node;
 
 public class Scope {
     private Scope parentScope;
-    private HashMap<Token, Node> members;
+    private HashMap<String, Node> members;
     
     public Scope(Scope scope)
     {
@@ -15,14 +15,34 @@ public class Scope {
     }
     public void add(Token name_token, Node node)
     {
-        if(members.containsKey(name_token))
+        String name = name_token.getIdentifier();
+        if(members.containsKey(name))
         {
-            System.err.println(String.format("file:%i:%s: %s", name_token.getLine(), "Duplicate variable", name_token.getIdentifier()));
+            error(name_token, "Duplicate variable");
         }
         else
         {
-            members.put(name_token, node);
+            members.put(name, node);
         }
+    }
+    public Node resolveName(Token nameToken)
+    {
+        Node node = members.get(nameToken.getIdentifier());
+        if(node != null)
+        {
+            return node;
+        }
+        if(parentScope != null)
+        {
+            return parentScope.resolveName(nameToken);
+        }
+        error(nameToken, "Undeclared variable");
+        return null;
+    }
+    private void error(Token token, String message)
+    {
+        System.err.println(String.format("file:%i:%s: %s", token.getLine(), message, token.getIdentifier()));
+
     }
 
 }
