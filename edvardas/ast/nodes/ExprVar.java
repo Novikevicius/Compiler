d5ff85586a582a3f50d85b782ad450d7c32d1a2b
Edvardas Novikevicius <edvardas.novikevicius@gmail.com>
2019-11-23 22:36:49 +0200

    added res names for var expr

--- edvardas/ast/nodes/ExprVar.java
+++ edvardas/ast/nodes/ExprVar.java
@@ -2,6 +2,7 @@ package edvardas.ast.nodes;
 
 import edvardas.Token;
 import edvardas.ast.ASTPrinter;
+import edvardas.parser.Scope;
 
 public class ExprVar extends Expression {
     private Token name;
@@ -11,5 +12,10 @@ public class ExprVar extends Expression {
     public void print(ASTPrinter printer) throws Exception
     {
         printer.print("name", name);
+    }
+    @Override
+    public void resolveNames(Scope scope)
+    {
+        scope.resolveName(name);
     }
 }
