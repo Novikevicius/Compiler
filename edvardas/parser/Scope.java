ec4e0e17ea63c67ff99b56ea5b457a3d855d5551
Edvardas Novikevicius <edvardas.novikevicius@gmail.com>
2019-12-01 20:25:26 +0200

    scope prints error to Main.error now

--- edvardas/parser/Scope.java
+++ edvardas/parser/Scope.java
@@ -2,51 +2,50 @@ package edvardas.parser;
 
 import java.util.HashMap;
 
+import edvardas.Main;
 import edvardas.Token;
 import edvardas.ast.nodes.Node;
 
 public class Scope {
     private Scope parentScope;
     private HashMap<String, Node> members;
-    
-    public Scope(Scope scope)
-    {
+
+    public Scope(Scope scope) {
         parentScope = scope;
         members = new HashMap<String, Node>();
     }
-    public Scope getParent()
-    {
+
+    public Scope getParent() {
         return parentScope;
     }
-    public void add(Token name_token, Node node)
-    {
+
+    public void add(Token name_token, Node node) {
         String name = name_token.getIdentifier();
-        if(members.containsKey(name))
-        {
+        if (members.containsKey(name)) {
             error(name_token, "Duplicate variable");
-        }
-        else
-        {
+        } else {
             members.put(name, node);
         }
     }
-    public Node resolveName(Token nameToken)
-    {
+
+    public Node resolveName(Token nameToken) {
         Node node = members.get(nameToken.getIdentifier());
-        if(node != null)
-        {
+        if (node != null) {
             return node;
         }
-        if(parentScope != null)
-        {
+        if (parentScope != null) {
             return parentScope.resolveName(nameToken);
         }
         error(nameToken, "Undeclared variable");
         return null;
     }
-    private void error(Token token, String message)
-    {
-        System.err.println(String.format("file:%d:%s: %s", token.getLine(), message, token.getIdentifier()));
+
+    private void error(Token token, String message) {
+        try {
+            Main.error(String.format("file:%d:%s: %s", token.getLine(), message, token.getIdentifier()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
