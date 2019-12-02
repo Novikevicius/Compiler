2cebb00cb7f424b3c3f60aec3bb887d292180a0a
Edvardas Novikevicius <edvardas.novikevicius@gmail.com>
2019-12-02 10:55:01 +0200

    fixed binary operation are allowed for non-number types

--- edvardas/ast/nodes/ExprBinArithm.java
+++ edvardas/ast/nodes/ExprBinArithm.java
@@ -13,6 +13,20 @@ public class ExprBinArithm extends ExprBinary {
     {
         Node t1 = left.checkTypes();
         Node t2 = right.checkTypes();
+        if(t1 instanceof TypePrim && t2 instanceof TypePrim)
+        {
+            TypePrim t = (TypePrim)t1;
+            switch(t.getKind())
+            {
+                case TYPE_INT:
+                case TYPE_FLOAT:
+                case TYPE_STRING:
+                    break;
+                default:
+                    semanticError(this.getLine(), "Binary operation is not allowed for " + t.getKind().getName());
+                    return null;
+            }
+        }
         unifyTypes(t1, t2, this.getLine());
         return t1;
     }
