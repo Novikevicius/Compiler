5f4d8bafd09b7b8ef053aa52be0d9197f49a19a4
Edvardas Novikevicius <edvardas.novikevicius@gmail.com>
2019-12-08 01:14:45 +0200

    implemented while loop code gen

--- edvardas/ast/nodes/StatementWhile.java
+++ edvardas/ast/nodes/StatementWhile.java
@@ -2,6 +2,9 @@ package edvardas.ast.nodes;
 
 import edvardas.State;
 import edvardas.ast.ASTPrinter;
+import edvardas.codeGeneration.CodeWriter;
+import edvardas.codeGeneration.Instruction;
+import edvardas.codeGeneration.Label;
 import edvardas.parser.Scope;
 
 public class StatementWhile extends Statement {
@@ -38,5 +41,16 @@ public class StatementWhile extends Statement {
     public int getLine()
     {
         return condition.getLine();
+    }
+    @Override
+    public void genCode(CodeWriter writer)
+    {
+        Label start = new Label();
+        Label end = new Label();
+        writer.placeLabel(start);
+        condition.genCode(writer);
+        writer.write(Instruction.JZ, end, State.TYPE_INT);
+        body.genCode(writer);
+        writer.placeLabel(end);
     }
 }
