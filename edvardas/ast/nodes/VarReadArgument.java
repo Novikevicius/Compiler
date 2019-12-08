e6a1517a2bffe690aa1ccd767e5f96d1ec746dd5
Edvardas Novikevicius <edvardas.novikevicius@gmail.com>
2019-12-08 23:09:16 +0200

    added var read argument code gen

--- edvardas/ast/nodes/VarReadArgument.java
+++ edvardas/ast/nodes/VarReadArgument.java
@@ -1,11 +1,16 @@
 package edvardas.ast.nodes;
 
+import edvardas.State;
 import edvardas.Token;
 import edvardas.ast.ASTPrinter;
+import edvardas.codeGeneration.CodeWriter;
+import edvardas.codeGeneration.Instruction;
 import edvardas.parser.Scope;
 
 public class VarReadArgument extends ReadArgument {
     private Token name;
+    public int stack_slot;
+    private State type;
 
     public VarReadArgument(Token name) {
         this.name = name;
@@ -17,7 +22,18 @@ public class VarReadArgument extends ReadArgument {
     @Override
     public void resolveNames(Scope s) 
     {
-        s.resolveName(name);
+        Node n = s.resolveName(name).parent;
+        if(n instanceof Decl)
+            stack_slot = ((Decl)n).stack_slot;
+        else
+        {
+            stack_slot = ((StatementVarDecl)n).getDeclaration().stack_slot;
+        }
+        try {
+			type = ((TypePrim)n.checkTypes()).getKind();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
     }
     @Override
     public Node checkTypes() throws Exception {
@@ -28,5 +44,10 @@ public class VarReadArgument extends ReadArgument {
     public int getLine()
     {
         return name.getLine();
+    }
+    @Override
+    public void genCode(CodeWriter writer)
+    {
+        writer.write(Instruction.PUSH, stack_slot, State.TYPE_INT);
     }
 }
