a4c8978b1021e515e661547504a26949865d1381
Edvardas Novikevicius <edvardas.novikevicius@gmail.com>
2019-12-08 23:13:48 +0200

    implemented array read argument gen code

--- edvardas/ast/nodes/ArrayReadArgument.java
+++ edvardas/ast/nodes/ArrayReadArgument.java
@@ -1,12 +1,16 @@
 package edvardas.ast.nodes;
 
+import edvardas.State;
 import edvardas.Token;
 import edvardas.ast.ASTPrinter;
+import edvardas.codeGeneration.CodeWriter;
+import edvardas.codeGeneration.Instruction;
 import edvardas.parser.Scope;
 
 public class ArrayReadArgument extends ReadArgument {
     private Token name;
     private Expression index;
+    private int stack_slot;
 
     public ArrayReadArgument(Token name, Expression index) {
         this.name = name;
@@ -21,7 +25,7 @@ public class ArrayReadArgument extends ReadArgument {
 
     @Override
     public void resolveNames(Scope scope) throws Exception {
-        scope.resolveName(name);
+        stack_slot = ((ArrayDeclaration)scope.resolveName(name)).stack_slot;
         index.resolveNames(scope);
     }
     @Override
@@ -33,5 +37,12 @@ public class ArrayReadArgument extends ReadArgument {
     public int getLine()
     {
         return name.getLine();
+    }
+    @Override
+    public void genCode(CodeWriter writer)
+    {
+        index.genCode(writer);
+        writer.write(Instruction.PUSH, stack_slot, State.TYPE_INT);
+        writer.write(Instruction.ADD, State.TYPE_INT);
     }
 }
