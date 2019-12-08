757aa527785723f088d1564444d490a70f3857fe
Edvardas Novikevicius <edvardas.novikevicius@gmail.com>
2019-12-08 21:48:22 +0200

    added instruction to set array elem value

--- edvardas/codeGeneration/Instruction.java
+++ edvardas/codeGeneration/Instruction.java
@@ -25,7 +25,8 @@ public enum Instruction {
     GET_L (0x0022, 1),
     SET_L (0x0023, 1),
     GET_A_L (0x0024, 0),
-    DEC_A_L (0x0025, 2),
+    SET_A_L (0x0025, 0),
+    DEC_A_L (0x0026, 2),
     // Control instructions
     RET (0x0030, 0),
     RET_V (0x0031, 0),
