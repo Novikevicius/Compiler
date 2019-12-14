ed528b346b02adec6326b39e7453ef9e5e1f19b2
Novikevicius <edvardas.novikevicius@gmail.com>
2019-12-14 17:57:48 +0200

    created Interpreter

---
+++ src/edvardas/Interpreter.java
@@ -0,0 +1,259 @@
+package edvardas;
+
+import java.util.ArrayList;
+
+import edvardas.codeGeneration.Instruction;
+
+class Interpreter{
+    private final int STACK_START = 1024;
+    private int[] memory;
+    private boolean running = false;
+    private int ip = 0;
+    private int fp = 0;
+    private int sp = 0;
+    private int type = -1;
+    public Interpreter(ArrayList<Integer> code)
+    {
+        memory = new int[4096];
+        for (int i = 0; i < code.size(); i++) {
+            memory[i] = code.get(i);
+        }
+        running = true;
+        fp = STACK_START;
+        sp = STACK_START;
+    }
+
+    public void execute()
+    {
+        while(running)
+        {
+            executeOne();
+        }
+        System.out.println(pop());
+    }
+    public void executeOne()
+    {
+        Instruction opcode = Instruction.instrByOpcode.get(readImm());
+        switch(opcode)
+        {
+            case ADD:
+            {
+                int a = pop();
+                int b = pop();
+                push(a + b);
+                break;
+            }
+            case SUB:
+            {
+                int a = pop();
+                int b = pop();
+                push(a - b);
+                break;
+            }
+            case MUL:
+            {
+                int a = pop();
+                int b = pop();
+                push(a * b);
+                break;
+            }
+            case DIV:
+            {
+                int a = pop();
+                int b = pop();
+                push(a / b);
+                break;
+            }
+            case EXP:
+            {
+                int a = pop();
+                int b = pop();
+                push(a ^ b);
+                break;
+            }
+            case NEG:
+            {
+                int a = pop();
+                push(-a);
+                break;
+            }
+            case INC:
+            {
+                int a = pop();
+                push(++a);
+                break;
+            }
+            case DEC:
+            {
+                int a = pop();
+                push(--a);
+                break;
+            }
+            case LESS:
+            {
+                int a = pop();
+                int b = pop();
+                push(a < b ? 1 : 0);
+                break;
+            }
+            case MORE:
+            {
+                int a = pop();
+                int b = pop();
+                push(a > b ? 1 : 0);
+                break;
+            }
+            case EQ:
+            {
+                int a = pop();
+                int b = pop();
+                push(a == b ? 1 : 0);
+                break;
+            }
+            case LEQ:
+            {
+                int a = pop();
+                int b = pop();
+                push(a <= b ? 1 : 0);
+                break;
+            }
+            case MEQ:
+            {
+                int a = pop();
+                int b = pop();
+                push(a >= b ? 1 : 0);
+                break;
+            }
+            case NEQ:
+            {
+                int a = pop();
+                int b = pop();
+                push(a != b ? 1 : 0);
+                break;
+            }
+            case AND:
+            {
+                int a = pop();
+                int b = pop();
+                push(a == b && a == 1 ? 1 : 0);
+                break;
+            }
+            case OR:
+            {
+                int a = pop();
+                int b = pop();
+                push(a == 1 || b == 1 ? 1 : 0);
+                break;
+            }
+            case PUSH:
+            {
+                int a = readArg();
+                push(a);
+                break;
+            }
+            case POP:
+            {
+                pop();
+                break;
+            }
+            case GET_L:
+            {
+                int i = readArg();
+                push(memory[fp+i]);
+                break;
+            }
+            case SET_L:
+            {
+                int i = readArg();
+                memory[fp+i] = pop();
+                break;
+            }
+            case ALLOC:
+            {
+                sp += readArg();
+                break;
+            }
+            case RET:
+            {
+                exec_ret(0);
+                break;
+            }
+            case RET_V:
+            {
+                exec_ret(pop());
+                break;
+            }
+            case CALL_BEGIN:
+            {
+                readArg();
+                push(0); push(0); push(0);
+                break;
+            }
+            case CALL:
+            {
+                exec_call(readArg(), readArg());
+                break;
+            }
+            case JMP:
+            {
+                ip = readArg();
+                break;
+            }
+            case JZ:
+            {
+                int i = readArg();
+                if(pop() == 0 ){
+                    ip = i;
+                }
+                break;
+            }
+            case EXIT:
+            {
+                running = false;
+                break;
+            }
+        }
+
+    }
+    private void exec_ret(int value)
+    {
+        int oldIP = memory[fp-3];
+        int oldFP = memory[fp-2];
+        int oldSP = memory[fp-1];
+        ip = oldIP;
+        fp = oldFP;
+        sp = oldSP;
+        push(value);
+    }
+    private void exec_call(int target, int arg_num)
+    {
+        int newIP = target;
+        int newFP = sp - arg_num;
+        int newSP = newFP;
+        memory[newFP - 3] = ip;
+        memory[newFP - 2] = fp;
+        memory[newFP - 1] = newFP - 3;
+        ip = newIP;
+        fp = newFP;
+        sp = newSP;
+    }
+    private void push(int v)
+    {
+        memory[sp++] = v;
+    }
+    private int pop()
+    {
+        return memory[--sp];
+    }
+    private int readImm()
+    {
+        int value = memory[ip++];
+        type = memory[ip++];
+        return value;
+    }
+    private int readArg()
+    {
+        int value = memory[ip++];
+        return value;
+    }
+}
