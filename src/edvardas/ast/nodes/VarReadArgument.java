c0e70f11d6a32698566415ab60905b3458d7a46b
Novikevicius <edvardas.novikevicius@gmail.com>
2019-12-14 22:58:47 +0200

    added var write to memory

--- src/edvardas/ast/nodes/VarReadArgument.java
+++ src/edvardas/ast/nodes/VarReadArgument.java
@@ -10,44 +10,49 @@ import edvardas.parser.Scope;
 public class VarReadArgument extends ReadArgument {
     private Token name;
     public int stack_slot;
-    private State type;
+    private Node type;
 
     public VarReadArgument(Token name) {
         this.name = name;
     }
-    public void print(ASTPrinter printer) throws Exception
-    {
+
+    public void print(ASTPrinter printer) throws Exception {
         printer.print("name", name);
     }
+
     @Override
-    public void resolveNames(Scope s) 
-    {
+    public void resolveNames(Scope s) {
         Node n = s.resolveName(name).parent;
-        if(n instanceof Decl)
-            stack_slot = ((Decl)n).stack_slot;
-        else
-        {
-            stack_slot = ((StatementVarDecl)n).getDeclaration().stack_slot;
+        if (n instanceof Decl)
+            stack_slot = ((Decl) n).stack_slot;
+        else {
+            stack_slot = ((StatementVarDecl) n).getDeclaration().stack_slot;
         }
         try {
-			type = ((TypePrim)n.checkTypes()).getKind();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+            type = ((TypePrim) n.checkTypes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
     @Override
     public Node checkTypes() throws Exception {
         // do nothing
-        return null;
+        return type;
     }
+
     @Override
-    public int getLine()
-    {
+    public int getLine() {
         return name.getLine();
     }
+
     @Override
-    public void genCode(CodeWriter writer)
-    {
-        writer.write(Instruction.PUSH, stack_slot, State.TYPE_INT);
+    public void genCode(CodeWriter writer) {
+        try {
+            writer.write(Instruction.READ, ((TypePrim) checkTypes()).getKind());
+            writer.write(Instruction.SET_L, stack_slot, ((TypePrim)type).getKind());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
